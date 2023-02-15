dd71591852bc87a4b48d1c7841cba8bebeb5c2f1
Adrian Plesner <adrian.plesner@gmail.com>
2023-02-15 15:01:55 +0100

    Implemented BT, FR and fasta in C#

---
+++ BenchmarkRunner/Benchmarks/C#/Binary Trees.cs
@@ -0,0 +1,82 @@
+using CsharpRAPL.Benchmarking.Attributes;
+using CsharpRAPL.Benchmarking.Attributes.Parameters;
+using CsharpRAPL.Benchmarking.Lifecycles;
+
+namespace BenchmarkRunner.Benchmarks.C_;
+
+using System;
+using System.Threading.Tasks;
+
+class BinaryTrees
+{
+    const int MinDepth = 4;
+    const int NoTasks = 4;
+    
+    [Benchmark("Binary trees","Binary trees in C#", name:"C sharp BT", skip: true)]
+    public static int BinaryTree([BenchmarkLoopiterations] ulong LoopIterations)
+    {
+        int maxDepth = 7;
+        int result = 0;
+
+        //Console.WriteLine(string.Concat("stretch tree of depth ", maxDepth + 1,"\t check: ", (bottomUpTree(maxDepth + 1)).itemCheck()));
+        var longLivedTree = bottomUpTree(maxDepth);
+        for (ulong i = 0; i < LoopIterations; i++)
+        {
+            
+
+            //var results = new string[(maxDepth - MinDepth) / 2 + 1];
+            int depth = maxDepth;
+            int n = (1 << depth + MinDepth) / NoTasks;
+            var tasks = new Task<int>[NoTasks];
+            for (int t = 0; t < tasks.Length; t++)
+            {
+                tasks[t] = Task.Run(() =>
+                {
+                    var check = 0;
+                    for (int i = n; i > 0; i--)
+                        check += (bottomUpTree(depth)).itemCheck();
+                    return check;
+                });
+            } 
+            var check = tasks[0].Result;
+            for (int t = 1; t < tasks.Length; t++)
+                check += tasks[t].Result;
+            result += check;
+        }
+        
+            //results[i] = string.Concat(n * NoTasks, "\t trees of depth ",
+              //  depth, "\t check: ", check);
+              //for (int i = 0; i < results.Length; i++)
+            //Console.WriteLine(results[i]);
+
+        //Console.WriteLine(string.Concat("long lived tree of depth ", maxDepth,"\t check: ", longLivedTree.itemCheck()));
+        return result;
+    }
+    
+    static TreeNode bottomUpTree(int depth) 
+    {
+        if (0 < depth) {
+            return new TreeNode(bottomUpTree(depth - 1), bottomUpTree(depth - 1));
+        }
+        return new TreeNode(null,null);
+    }
+
+    private class TreeNode
+    {
+        readonly TreeNode? left, right;
+        
+        internal TreeNode(TreeNode left, TreeNode right) 
+        {
+            this.left = left;
+            this.right = right;
+        }     
+
+        internal int itemCheck()
+        {
+            if (left == null) {
+                return 1;
+            }
+            return 1 + left.itemCheck() + right.itemCheck();
+        }
+    }   
+}
--- BenchmarkRunner/Program.cs
+++ BenchmarkRunner/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using CsharpRAPL.Benchmarking;
+using CsharpRAPL.CommandLine;
 
-Console.WriteLine("Hello, World!");
+var options = CsharpRAPLCLI.Parse(args);
+options.PlotResults = true;
+options.ZipResults = true;
+
+var suite = new BenchmarkCollector(options.Iterations, options.LoopIterations);
+suite.RunAll(false);

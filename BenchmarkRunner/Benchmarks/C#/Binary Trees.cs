e5f0f57ca7c3f221b84aef04d1574d99fb761adb
Adrian Plesner <adrian.plesner@gmail.com>
2023-02-28 14:54:59 +0100

    fix

--- BenchmarkRunner/Benchmarks/C#/Binary Trees.cs
+++ BenchmarkRunner/Benchmarks/C#/Binary Trees.cs
@@ -13,9 +13,9 @@ public class BinaryTrees
     const int NoTasks = 4;
     
     [Benchmark("Binary Trees","Binary trees in C#", name:"C sharp BT", skip: false)]
-    public static int BinaryTree([BenchmarkLoopiterations] ulong LoopIterations, int maxDepth = 7)
+    public static int BinaryTree([BenchmarkLoopiterations] ulong LoopIterations)
     {
-        //int maxDepth = 7;
+        int maxDepth = 7;
         int result = 0;
 
         //Console.WriteLine(string.Concat("stretch tree of depth ", maxDepth + 1,"\t check: ", (bottomUpTree(maxDepth + 1)).itemCheck()));
